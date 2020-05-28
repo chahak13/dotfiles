26746fc403d6e7fa2d72ccf8da02039e5f02b4b7
Chahak Mehta <chahak.mehta@proptiger.com>
2020-05-28 12:36:16 +0530

    Change path to save tmux sessions

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,6 +9,12 @@ alias dotfiles="cd ~/.dotfiles"
 alias cp='cp -i'
 alias mv='mv -i'
 
+# Change tmux directory to save server info
+if ! [[ -d $HOME/.tmux_sessions ]];then
+    mkdir $HOME/.tmux_sessions
+fi
+export TMUX_TMPDIR=$HOME/.tmux_sessions
+
 # Mysql aliases
 alias mysql='/usr/local/mysql/bin/mysql'
 alias mysqladmin='/usr/local/mysql/bin/mysqladmin'
