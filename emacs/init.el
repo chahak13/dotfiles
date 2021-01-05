2ca764e26d7b57f452b5188e4b05c0ccc49bd7c6
chahak13 <chahak.mehta013@gmail.com>
2021-01-05 21:56:39 +0530

    [EMACS] Add FiraGo as the variable pitch font

--- emacs/init.el
+++ emacs/init.el
@@ -69,7 +69,8 @@
 ;; 	    (add-hook 'after-save-hook 'black-format-python-file nil 'local)))
 
 (defun cm/set-font-faces ()
-  (set-face-attribute 'default nil :font "Hack" :height cm/default-font-size))
+  (set-face-attribute 'default nil :font "Hack" :height cm/default-font-size)
+  (set-face-attribute 'variable-pitch nil :font "FiraGo" :height cm/default-variable-font-size))
 
 ;; =====
 ;; Hooks
