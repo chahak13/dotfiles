ab9a0306bccf681b6245af84351c3f2541fd9b3b
chahak13 <chahak.mehta013@gmail.com>
2021-01-05 21:57:35 +0530

    [EMACS] Add olivetti, mu4e and pdf-tools
    For an easier time reading files on Emacs, I'm adding the Olivetti
    package for a better experience. Furthermore, I've also added a
    rudimentary config for mu4e which I'm still setting up.
    
    I've also added the pdf-tools package for better pdf viewing.

--- emacs/init.el
+++ emacs/init.el
@@ -276,3 +276,40 @@
          ("C-x 4 C-j" . dired-jump-other-window)
          :map dired-mode-map
          ("I" . dired-info)))
+
+;; Olivetti mode
+(use-package olivetti
+  :straight t
+  :diminish
+  :config
+  (setq olivetti-body-width 0.7)
+  (setq olivetti-minimum-body-width 80)
+  (setq olivetti-recall-visual-line-mode-entry-state t))
+
+;; Mu4e Email settings
+(use-package mu4e
+  :load-path "/usr/share/emacs/site-lisp/mu4e"
+  :config
+  ;; This is set to 't' to avoid mail syncing issues when using mbsync
+  (setq mu4e-change-filenames-when-moving t)
+
+  ;; Refresh mail using isync every 10 minutes
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq mu4e-maildir "~/Mail")
+
+  (setq mu4e-drafts-folder "/[Gmail]/Drafts")
+  (setq mu4e-sent-folder   "/[Gmail]/Sent Mail")
+  (setq mu4e-refile-folder "/[Gmail]/All Mail")
+  (setq mu4e-trash-folder  "/[Gmail]/Trash")
+
+  (setq mu4e-maildir-shortcuts
+	'((:maildir "/Inbox"    :key ?i)
+	  (:maildir "/[Gmail]/Sent Mail" :key ?s)
+	  (:maildir "/[Gmail]/Trash"     :key ?t)
+	  (:maildir "/[Gmail]/Drafts"    :key ?d)
+	  (:maildir "/[Gmail]/All Mail"  :key ?a))))
+
+;; PDF tools
+(use-package pdf-tools
+  :straight t)
