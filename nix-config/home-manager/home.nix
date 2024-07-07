518df6e0ba4016b7342fe05711b43a9980c2b527
Chahak Mehta <chahakmehta013@gmail.com>
2024-07-07 15:14:01 -0400

    Use fish with vi mode

--- nix-config/home-manager/home.nix
+++ nix-config/home-manager/home.nix
@@ -67,8 +67,17 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      zoxide init fish | source
-      alias ls eza
+    zoxide init fish | source
+    alias ls eza
+
+    function fish_hybrid_key_bindings --description \
+    "Vi-style bindings that inherit emacs-style bindings in all modes"
+        for mode in default insert visual
+            fish_default_key_bindings -M $mode
+        end
+        fish_vi_key_bindings --no-erase
+    end
+    set -g fish_key_bindings fish_hybrid_key_bindings
     '';
   };
 
