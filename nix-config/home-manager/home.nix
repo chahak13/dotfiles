15a1bbba75630aeb4e23f3b23a2ec2b0c1ce1595
Chahak Mehta <chahakmehta013@gmail.com>
2024-07-07 17:59:56 -0400

    Add config for mako

--- nix-config/home-manager/home.nix
+++ nix-config/home-manager/home.nix
@@ -124,7 +124,38 @@
     };
   };
 
-  services.mako.enable = true;
+  services.mako = {
+    enable = true;
+    sort = "-time";
+    layer = "overlay";
+    backgroundColor = "#2e3440";
+    width = 300;
+    height = 110;
+    borderSize = 2;
+    borderColor = "#88c0d0";
+    borderRadius = 15;
+    icons = true;
+    maxIconSize = 64;
+    defaultTimeout = 5000;
+    ignoreTimeout = true;
+    font = "monospace 14";
+    extraConfig = ''
+    [urgency=low]
+    border-color=#cccccc
+
+    [urgency=normal]
+    border-color=#d08770
+
+    [urgency=high]
+    border-color=#bf616a
+    default-timeout=0
+
+    [category=mpd]
+    default-timeout=2000
+    group-by=category
+    '';
+  };
+
   services.poweralertd.enable = true;
 
   # Nicely reload system units when changing configs
